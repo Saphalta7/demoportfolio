ff4c8c3b99866151776c2c8059aff92d2eea7d7a
Saphalta <100756573+Saphalta7@users.noreply.github.com>
2023-06-13 23:34:06 -0500

    Add files via upload

---
+++ 0FinalProjectWebsite/sheet.js
@@ -0,0 +1,10 @@
+function myFunction(){
+	document.body.style.backgroundColor="Silver";}
+function myfunction(){
+	document.body.style.backgroundColor="Tan";}
+function mFunction(){
+	document.body.style.backgroundColor="LightPink";}
+
+
+
+
